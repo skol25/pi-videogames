1b67c53c6dbb2f31e6db1f148a40b45d184d9f1d
skol25 <andres.lobolugo25@gmail.com>
2022-11-11 00:45:57 -0400

    endpoints

--- api/src/routes/videogames.router.js
+++ api/src/routes/videogames.router.js
@@ -10,14 +10,16 @@ const axios = require('axios')
 /**
  * 
  * por params ruta /:id
- * por query ?name="..."
+ * por query ?name=...&
  * por body viene un objeto por body
  */
-/**
- * devule todos los videojuegos
+/*
+ * buscar videojuego
  *  ruta : /api/v1/videogames
+ * @params {string} name - nombre del videojuego - query
  * 
  */
+
 router.get('/',async (req,res)=>{
         
         try {
@@ -32,15 +34,51 @@ router.get('/',async (req,res)=>{
         
   
 })
-
-router.get('/',async(req,res)=>{
+/*
+ * detalle de videojuego
+ *  ruta : /api/v1/videogames
+ * @params {string} id -id del videojuego a buscar - params
+ * 
+ * devuelve
+ * [ ] Los campos mostrados en la ruta principal para cada videojuegos (imagen, nombre, y géneros)
+ * [ ] Descripción
+ * [ ] Fecha de lanzamiento
+ * [ ] Rating
+ * [ ] Plataformas
+ */
+router.get('/:id',async(req,res)=>{
         try {
-                
+                const {id} = req.params;
+                const videogameDetail = await videogamesService.detailVideogame(id)
+                res.status(200).json(videogameDetail)
+
 
         } catch (error) {
                 
+                res.status(400).json({message:error.message})
         }
 
+})
+/**
+ * Nombre
+Descripción
+Fecha de lanzamiento
+Rating
+[ ] Posibilidad de seleccionar/agregar varios géneros
+[ ] Posibilidad de seleccionar/agregar varias plataformas
+ */
+router.post('/',async (req,res)=>{
+        try {
+                const {name,description,released,rating,genres,platforms} = req.body
+
+                const gameCreated = await videogamesService.createVideogame({name,description,released,rating,genres,platforms}) //probar este objeto a ver si llega 
+                res.status(200).json({
+                        message:'el juego fue creado',
+                        data:gameCreated //devolver el juego creado basicamente mismo formulario 
+                })
+        } catch (error) {
+                res.status(400).json({message:error.message})
+        }
 })
 
 module.exports= router
--- api/src/services/videogames.service.js
+++ api/src/services/videogames.service.js
@@ -3,14 +3,7 @@ require('dotenv').config();
 const {
     API_KEY
 } = process.env;
-/**
- * logica de videogames 
- * [ ] Los campos mostrados en la ruta principal para cada videojuegos (imagen, nombre, y géneros)
-    [ ] Descripción
-[ ] Fecha de lanzamiento
-[ ] Rating
-[ ] Plataformas
- */
+
 
 module.exports = {
     /**
@@ -23,11 +16,10 @@ module.exports = {
         
         try {
             if(!name){
-                console.log('vengo sin name en query')
                 const response= await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
             
             if(response){
-                const filterlist =response.data.results.map(element=>{
+                const filterlist = response.data.results.map(element=>{
                     return{
                         id:element.id,
                         name:element.name,
@@ -49,12 +41,16 @@ module.exports = {
             }
             else{
                 /*si viene el nombre del juego por query entonces */
-                console.log(name)
+            
                 const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${name}`)
-                if(response){
+
+                //el response devuelve un array y dicho caso que no venga con nada el array viene vacio 
+                if(response.data.results.length!=0){
+                    
                     return response.data.results
                 }else{
-                    throw Error('no existe el juego buscado')
+    
+                    throw Error(`el videojuego ${name} no fue encontrado.`)
                 }
                 
             }
@@ -66,20 +62,55 @@ module.exports = {
       
     },
 
-    videogameDetail: async function(){
+    /**
+     * DETALLE DE UN VIDEOJUEGO
+     * * [ ] Los campos mostrados en la ruta principal para cada videojuegos (imagen, nombre, y géneros)
+    * [ ] Descripción listo
+    * [ ] Fecha de lanzamiento listo
+    * [ ] Rating listo
+    * [ ] Plataformas listo
+     */
+    detailVideogame: async function(idVideogame){
 
         try {
-            
+            const response = await axios.get(`https://api.rawg.io/api/games/${idVideogame}?key=${API_KEY}`)
+            if(response.data){
+                let element = response.data
+                const detailVideogame = {
 
+                    name : element.name,
+                    image : element.background_image,
+                    released : element.released,
+                    rating : element.rating,
+                    description : element.description,
+                    platforms : element.parent_platforms,
+                    genres : element.genres 
+                
 
-
-
+                }
+                return detailVideogame
+            }
+            else{
+                throw Error('El id no coincide con ningun juego guardado.')
+            }
         } catch (error) {
+           
             throw Error(error.message)
         }
 
-
     },
+    createVideogame:async function(form){
+
+        try {
+            
+            //falta traer el videjuego y guardarlo en la base de datos 
+
+        } catch (error) {
+            throw Error('error al crear el videojuego.')
+        }
+
+    }
+
 
 
 } 
