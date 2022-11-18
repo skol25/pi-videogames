a991c6a88aca29c1cdc16aed63b551b8a6a0386e
skol25 <andres.lobolugo25@gmail.com>
2022-11-18 01:31:09 -0400

    centrado contenido formulario

--- client/src/modules/createVideogameComponent/createVideogameComponent.js
+++ client/src/modules/createVideogameComponent/createVideogameComponent.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import './createVideogameComponent.css'
 
 export default function CreateVideogameComponent() {
   return (
-    <div>formulario</div>
+    <React.Fragment>
+        <div className='form-container'>
+
+            <div>formulario</div>
+            <div>formulario</div>
+            <div>formulario</div>
+            <div>formulario</div>
+
+        </div>
+
+
+
+    </React.Fragment>
   )
 }
