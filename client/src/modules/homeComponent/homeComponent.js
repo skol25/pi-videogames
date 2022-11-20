01a285f643fd86f8beba0f63befc199da9f2a3c2
skol25 <andres.lobolugo25@gmail.com>
2022-11-20 01:33:14 -0400

    spinner y search

--- client/src/modules/homeComponent/homeComponent.js
+++ client/src/modules/homeComponent/homeComponent.js
@@ -3,34 +3,56 @@ import {  useDispatch, useSelector } from 'react-redux'
 import { getAllVideogames } from '../../redux/actions'
 
 import CardComponent from '../../shared/components/cardComponent/cardComponent'
+import SpinnerComponent from '../../shared/components/spinnerComponent/spinnerComponent'
+
+import searchIcon from '../../shared/images/icons/searchIcon.svg'
+
 import './homeComponent.css'
 
 export default function HomeComponent() {
 
   const disp = useDispatch()
+  let [spinner,setSpinner] = useState(true)
+  let [allVideogames,setvideogames] = useState([])
+  let [search,setSearch] = useState([])
 
-  
+
+
    let getVideogames = useSelector((state) => state.videogames)
 
-  let [allVideogames,setvideogames] = useState([])
+   useEffect(()=>{
+    (async () => {
+      const data = await disp(getAllVideogames());
+      
+      setSpinner(false);
+      
+   })();
+
+   } ,[disp,allVideogames])
+
+
+  
+  // useEffect(()=>{
+    
+   
+  //   disp(getAllVideogames())
+  //   setvideogames(getVideogames)
   
 
-  useEffect(()=>{
-
-    disp(getAllVideogames())
-    
-  },[])
-
- let click = ()=>{
-  console.log()
- }
+  // },[disp,allVideogames])
 
 
   
   return (
     <React.Fragment>
+      <div className='home-input'>
+        <input className='home-input-'/>
+        <img src={searchIcon} />     
+      </div>
+      {spinner && <SpinnerComponent/>}
     
       <div className='container'>  
+
         { getVideogames && getVideogames.map( (videogame) => {
 
           return(
@@ -38,7 +60,6 @@ export default function HomeComponent() {
             <CardComponent id={videogame.id} name={videogame.name} image={videogame.image}  genres={videogame.genres} />
 
             </div>
-
           );
 
         })}
---
+++ client/src/shared/components/spinnerComponent/spinnerComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './spinnerComponent.css'
+
+export default function SpinnerComponent() {
+  return (
+    <React.Fragment>
+      <div className='spiner-bg'>
+
+      <div class="spinner">
+        <div class="dot1"></div>
+        <div class="dot2"></div>
+        <div class="dot3"></div>
+      </div>
+
+      </div>
+
+    </React.Fragment>
+  )
+}
